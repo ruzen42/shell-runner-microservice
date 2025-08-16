d5455a066de97b3041ccc760bed39488c7f4113d
ruzen42 <malledeferlive@gmail.com>
2025-08-16 08:47:08 +0500

    change API

--- Models/CommandResponse.cs
+++ Models/CommandResponse.cs
@@ -3,8 +3,6 @@ namespace ShellRunner.Models;
 public class CommandResponse
 {
     public string? Output { get; set; }
-    public required CommandRequest Context { get; set; }
-    
     public string? Error { get; set; }
     public required int ExitCode { get; init; }
 }
