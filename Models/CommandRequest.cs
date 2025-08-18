bdc5113cb354b1697fdee7b4fcc4412b80c1b834
ruzen42 <malledeferlive@gmail.com>
2025-08-18 15:12:59 +0500

    fix bugs

--- Models/CommandRequest.cs
+++ Models/CommandRequest.cs
@@ -1,6 +1,6 @@
 namespace ShellRunner.Models;
 
-public class CommandRequest
+public record CommandRequest
 {
     public required string Command { get; set; }
     public override string ToString() => $"Command: {Command}\n userId: {UserId}\n from chat: {ChatId}";
