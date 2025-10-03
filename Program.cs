d773fe6f092d16b485ff0c7d95dea842e2523029
ruzen42 <malledeferlive@gmail.com>
2025-10-03 17:41:31 +0500

    remove redundant

--- Program.cs
+++ Program.cs
@@ -11,12 +11,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Настройка Swagger UI
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-});
+app.UseSwaggerUI();
 
 app.MapControllers();
 
