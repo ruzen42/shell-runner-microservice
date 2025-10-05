36951e411fcc9f3f6439599884ca47be9e9f68f7
ruzen42 <malledeferlive@gmail.com>
2025-10-05 14:34:18 +0500

    fix swagger

--- Program.cs
+++ Program.cs
@@ -13,8 +13,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
 
 app.MapControllers();
 
